1e08b4f197e4ee3b03f7365b09054f1cffa474ee
Mukilan K <mukilankumar003@gmail.com>
2025-02-08 08:04:05 +0530

    butn fix

--- src/components/careerPage/currentOpening.jsx
+++ src/components/careerPage/currentOpening.jsx
@@ -34,7 +34,7 @@ export default function CurrentOpenings() {
                                 <p className="text-lightGray font-barlow text-[13.32px] font-normal mb-12">Bring your creativity and expertise to our team as a UI Designer. Collaborate with cross-functional teams to design visually stunning and user-friendly interfaces. Utilize your skills in layout design, typography, and color theory to create engaging digital experiences that leave a lasting impression.</p>
                             </div>
                             <a target="__blank" href="https://docs.google.com/forms/d/e/1FAIpQLScTgnb92nIaTh_c32DD-uLvnkiKI0p_V_4GBl4qxcPeb3UNSg/viewform?usp=header">
-                                <button className="w-1/2 sm:w-full bg-lightOrange text-white py-2 px-6 rounded-md font-semibold self-start">
+                                <button className=" sm:w-full bg-lightOrange text-white py-2 px-6 rounded-md font-semibold self-start">
                                     Apply Now
                                 </button>
                             </a>
@@ -56,7 +56,7 @@ export default function CurrentOpenings() {
                                 <p className="text-lightGray font-barlow text-[13.32px] font-normal mb-12">Join us as a UX Designer and help shape exceptional user experiences. Conduct user research, analyze data, and create wireframes and prototypes to design intuitive and user-centric interfaces. Collaborate closely with UI Designers, developers, and stakeholders to ensure seamless and enjoyable user journeys.</p>
                             </div>
                             <a target="__blank" href="https://docs.google.com/forms/d/e/1FAIpQLScTgnb92nIaTh_c32DD-uLvnkiKI0p_V_4GBl4qxcPeb3UNSg/viewform?usp=header">
-                                <button className="w-1/2 sm:w-full bg-lightOrange text-white py-2 px-6 rounded-md font-semibold self-start">
+                                <button className=" sm:w-full bg-lightOrange text-white py-2 px-6 rounded-md font-semibold self-start">
                                     Apply Now
                                 </button>
                             </a>
@@ -88,7 +88,7 @@ export default function CurrentOpenings() {
                                 <p className="text-lightGray font-barlow text-[13.32px] font-normal mb-12">Lead our design team as a Design Head and drive the creative vision of our products. Provide strategic direction, mentorship, and guidance to UI and UX designers. Collaborate with cross-functional teams to ensure design consistency and elevate our brand identity through innovative and visually impactful designs.</p>
                             </div>
                             <a target="__blank" href="https://docs.google.com/forms/d/e/1FAIpQLScTgnb92nIaTh_c32DD-uLvnkiKI0p_V_4GBl4qxcPeb3UNSg/viewform?usp=header">
-                                <button className="w-1/2 sm:w-full bg-lightOrange text-white py-2 px-6 rounded-md font-semibold self-start">
+                                <button className=" sm:w-full bg-lightOrange text-white py-2 px-6 rounded-md font-semibold self-start">
                                     Apply Now
                                 </button>
                             </a>
@@ -119,7 +119,7 @@ export default function CurrentOpenings() {
                                 <p className="text-lightGray font-barlow text-[13.32px] font-normal mb-12">Join our team as a Front-End Developer and bring our designs to life. Transform UI/UX wireframes into interactive web interfaces using HTML, CSS, and JavaScript. Collaborate closely with designers and back-end developers to ensure seamless integration and optimal user experiences.</p>
                             </div>
                             <a target="__blank" href="https://docs.google.com/forms/d/e/1FAIpQLScTgnb92nIaTh_c32DD-uLvnkiKI0p_V_4GBl4qxcPeb3UNSg/viewform?usp=header">
-                                <button className="w-1/2 sm:w-full bg-lightOrange text-white py-2 px-6 rounded-md font-semibold self-start">
+                                <button className=" sm:w-full bg-lightOrange text-white py-2 px-6 rounded-md font-semibold self-start">
                                     Apply Now
                                 </button>
                             </a>
@@ -141,7 +141,7 @@ export default function CurrentOpenings() {
                                 <p className="text-lightGray font-barlow text-[13.32px] font-normal mb-12">Be part of our team as a Backend Developer and contribute to building robust and scalable web applications. Develop server-side logic, integrate databases, and optimize system performance. Collaborate with front-end developers to ensure smooth communication between the server and the user interface.</p>
                             </div>
                             <a target="__blank" href="https://docs.google.com/forms/d/e/1FAIpQLScTgnb92nIaTh_c32DD-uLvnkiKI0p_V_4GBl4qxcPeb3UNSg/viewform?usp=header">
-                                <button className="w-1/2 sm:w-full bg-lightOrange text-white py-2 px-6 rounded-md font-semibold self-start">
+                                <button className=" sm:w-full bg-lightOrange text-white py-2 px-6 rounded-md font-semibold self-start">
                                     Apply Now
                                 </button>
                             </a>
@@ -166,7 +166,7 @@ export default function CurrentOpenings() {
                                 <p className="text-lightGray font-barlow text-[13.32px] font-normal mb-12">Join us as a Full Stack Developer and take on end-to-end responsibility for web application development. Combine your skills in both front-end and back-end technologies to create dynamic and responsive websites. Collaborate with designers, developers, and stakeholders to deliver comprehensive and user-friendly solutions.</p>
                             </div>
                             <a target="__blank" href="https://docs.google.com/forms/d/e/1FAIpQLScTgnb92nIaTh_c32DD-uLvnkiKI0p_V_4GBl4qxcPeb3UNSg/viewform?usp=header">
-                                <button className="w-1/2 sm:w-full bg-lightOrange text-white py-2 px-6 rounded-md font-semibold self-start">
+                                <button className=" sm:w-full bg-lightOrange text-white py-2 px-6 rounded-md font-semibold self-start">
                                     Apply Now
                                 </button>
                             </a>
@@ -197,7 +197,7 @@ export default function CurrentOpenings() {
                                 <p className="text-lightGray font-barlow text-[13.32px] font-normal mb-12">Lead our business analysis team as a BA Manager and drive strategic initiatives. Gather and analyze requirements, facilitate communication between stakeholders, and ensure project alignment with business objectives. Provide leadership and mentorship to a team of talented business analysts.</p>
                             </div>
                             <a target="__blank" href="https://docs.google.com/forms/d/e/1FAIpQLScTgnb92nIaTh_c32DD-uLvnkiKI0p_V_4GBl4qxcPeb3UNSg/viewform?usp=header">
-                                <button className="w-1/2 sm:w-full bg-lightOrange text-white py-2 px-6 rounded-md font-semibold self-start">
+                                <button className=" sm:w-full bg-lightOrange text-white py-2 px-6 rounded-md font-semibold self-start">
                                     Apply Now
                                 </button>
                             </a>
@@ -222,7 +222,7 @@ export default function CurrentOpenings() {
                                 <p className="text-lightGray font-barlow text-[13.32px] font-normal mb-12">Join our team as a Project Manager and oversee the successful delivery of projects from initiation to completion. Define project scope, manage timelines and resources, and ensure effective communication across cross-functional teams. Utilize your leadership and organizational skills to drive project success.</p>
                             </div>
                             <a target="__blank" href="https://docs.google.com/forms/d/e/1FAIpQLScTgnb92nIaTh_c32DD-uLvnkiKI0p_V_4GBl4qxcPeb3UNSg/viewform?usp=header">
-                                <button className="w-1/2 sm:w-full bg-lightOrange text-white py-2 px-6 rounded-md font-semibold self-start">
+                                <button className=" sm:w-full bg-lightOrange text-white py-2 px-6 rounded-md font-semibold self-start">
                                     Apply Now
                                 </button>
                             </a>
@@ -249,7 +249,7 @@ export default function CurrentOpenings() {
                                 <p className="text-lightGray font-barlow text-[13.32px] font-normal mb-12">Be part of our team as an HR Manager and play a vital role in managing our human resources. Lead talent acquisition, employee engagement, and performance management initiatives. Collaborate with leadership to develop and implement HR strategies that foster a positive and inclusive work culture.</p>
                             </div>
                             <a target="__blank" href="https://docs.google.com/forms/d/e/1FAIpQLScTgnb92nIaTh_c32DD-uLvnkiKI0p_V_4GBl4qxcPeb3UNSg/viewform?usp=header">
-                                <button className="w-1/2 sm:w-full bg-lightOrange text-white py-2 px-6 rounded-md font-semibold self-start">
+                                <button className=" sm:w-full bg-lightOrange text-white py-2 px-6 rounded-md font-semibold self-start">
                                     Apply Now
                                 </button>
                             </a>
@@ -279,7 +279,7 @@ export default function CurrentOpenings() {
                                 <p className="text-lightGray font-barlow text-[13.32px] font-normal mb-12">Ensure the quality of our software products as a QA Tester. Develop test plans, execute test cases, and identify and report software defects. Collaborate with developers and stakeholders to ensure that our products meet high-quality standards and deliver an exceptional user experience.</p>
                             </div>
                             <a target="__blank" href="https://docs.google.com/forms/d/e/1FAIpQLScTgnb92nIaTh_c32DD-uLvnkiKI0p_V_4GBl4qxcPeb3UNSg/viewform?usp=header">
-                                <button className="w-1/2 sm:w-full bg-lightOrange text-white py-2 px-6 rounded-md font-semibold self-start">
+                                <button className=" sm:w-full bg-lightOrange text-white py-2 px-6 rounded-md font-semibold self-start">
                                     Apply Now
                                 </button>
                             </a>
@@ -301,7 +301,7 @@ export default function CurrentOpenings() {
                                 <p className="text-lightGray font-barlow text-[13.32px] font-normal mb-12">Join us as an SQL Tester and play a key role in testing and validating the integrity of our databases. Write complex SQL queries to perform data validation and identify any anomalies. Collaborate with developers and QA testers to ensure the accuracy and reliability of our data.</p>
                             </div>
                             <a target="__blank" href="https://docs.google.com/forms/d/e/1FAIpQLScTgnb92nIaTh_c32DD-uLvnkiKI0p_V_4GBl4qxcPeb3UNSg/viewform?usp=header">
-                                <button className="w-1/2 sm:w-full bg-lightOrange text-white py-2 px-6 rounded-md font-semibold self-start">
+                                <button className=" sm:w-full bg-lightOrange text-white py-2 px-6 rounded-md font-semibold self-start">
                                     Apply Now
                                 </button>
                             </a>
@@ -323,7 +323,7 @@ export default function CurrentOpenings() {
                                 <p className="text-lightGray font-barlow text-[13.32px] font-normal mb-12">Be part of our team as a Manual Tester and perform comprehensive manual testing to ensure the quality and functionality of our software applications. Develop test cases, execute test scripts, and document test results. Collaborate with developers and QA testers to troubleshoot issues and enhance software performance.</p>
                             </div>
                             <a target="__blank" href="https://docs.google.com/forms/d/e/1FAIpQLScTgnb92nIaTh_c32DD-uLvnkiKI0p_V_4GBl4qxcPeb3UNSg/viewform?usp=header">
-                                <button className="w-1/2 sm:w-full bg-lightOrange text-white py-2 px-6 rounded-md font-semibold self-start">
+                                <button className=" sm:w-full bg-lightOrange text-white py-2 px-6 rounded-md font-semibold self-start">
                                     Apply Now
                                 </button>
                             </a>
